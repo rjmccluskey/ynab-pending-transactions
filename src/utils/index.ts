10c52b8dd42892e570ec05ac1d547e0d0c98fbd6
rjmccluskey <rjmccluskey@gmail.com>
2020-02-02 21:39:18 -0800

    retry once if function fails

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,12 @@
 export function handledAsync<T>(func: () => Promise<T>,
                                 onError: (e: Error) => any): () => Promise<T> {
   return async () => {
-    return func().catch(onError);
+    try {
+      return await func().catch(onError);
+    } catch (e) {
+      console.log(`Failed on first try with error message: ${e.message}`);
+      console.log('Retrying...');
+      return func().catch(onError);
+    }
   }
 }
