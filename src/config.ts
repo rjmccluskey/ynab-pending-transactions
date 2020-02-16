b49c226416ab268175bb712199d531bc9ef99094
rjmccluskey <rjmccluskey@gmail.com>
2020-02-16 09:02:15 -0800

    configure node environment

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,18 @@
-function getEnvVar(name: string): string {
+function getEnvVar(name: string): string
+function getEnvVar(name: string, required: true): string
+function getEnvVar(name: string, required: false): string|undefined
+function getEnvVar(name: string, required = true): string|undefined {
   const value = process.env[name];
-  if (value === undefined) {
+  if (required && value === undefined) {
     throw new Error(`Missing env variable ${name}`);
   }
   return value;
+}
+
+export enum NodeEnv {
+  dev = 'development',
+  prod = 'production',
+  ci = 'CI'
 }
 
 export const config = {
@@ -11,5 +20,6 @@ export const config = {
   wfUrl: getEnvVar('WF_URL'),
   wfUsername: getEnvVar('WF_USERNAME'),
   wfPassword: getEnvVar('WF_PASSWORD'),
-  captchaToken: getEnvVar('CAPTCHA_TOKEN')
+  captchaToken: getEnvVar('CAPTCHA_TOKEN'),
+  nodeEnv: (getEnvVar('NODE_ENV', false) || NodeEnv.prod) as NodeEnv
 };
