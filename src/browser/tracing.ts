54fbfa4968fc02582ef19696c47399714e9f20f1
rjmccluskey <rjmccluskey@gmail.com>
2020-04-19 15:41:01 -0700

    Log console messages when tracing

--- src/browser/tracing.ts
+++ src/browser/tracing.ts
@@ -1,33 +1,64 @@
 import { Page } from 'puppeteer';
-import { config, NodeEnv } from '../config';
+import { config } from '../config';
 import { storeBuffer } from '../storage';
+import * as fs from 'fs';
+
+export async function startTracing(page: Page): Promise<void> {
+  await Promise.all([
+    startBrowserTracing(page),
+    startConsoleTracing(page),
+  ]);
+}
+
+export async function stopTracing(): Promise<void> {
+  await Promise.all([
+    stopBrowserTracing(),
+    stopConsoleTracing(),
+  ]);
+}
 
 let tracedPage: Page|null = null;
 
-export async function startTracing(page: Page): Promise<void> {
+async function startBrowserTracing(page: Page): Promise<void> {
   if (!config.useTracing || tracedPage) {
     return;
   }
 
   tracedPage = page;
 
-  let traceFilename = 'trace.json';
-  if (config.nodeEnv === NodeEnv.prod) {
-    traceFilename = `/tmp/${traceFilename}`;
-  }
-
   return page.tracing.start({
-    path: traceFilename,
+    path: config.browserTraceFilePath,
     screenshots: true
   });
 }
 
-export async function stopTracing(): Promise<void> {
+async function stopBrowserTracing(): Promise<void> {
   const buffer = await tracedPage?.tracing.stop();
 
   if (buffer && config.debugBucketName) {
     const date = new Date().toISOString();
     await storeBuffer(buffer, config.debugBucketName, `${date}-trace.json`);
     tracedPage = null;
+  }
+}
+
+async function startConsoleTracing(page: Page): Promise<void> {
+  // Clear out the file if it already exists
+  fs.writeFileSync(config.consoleTraceFilePath, '');
+
+  page.on('console', message => {
+    fs.appendFileSync(config.consoleTraceFilePath, message.text() + "\n");
+  });
+}
+
+async function stopConsoleTracing(): Promise<void> {
+  if (!config.debugBucketName) {
+    return;
+  }
+
+  const buffer = fs.readFileSync(config.consoleTraceFilePath);
+  if (buffer) {
+    const date = new Date().toISOString();
+    await storeBuffer(buffer, config.debugBucketName, `${date}-console.log`);
   }
 }
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 function getEnvVar(name: string): string
-function getEnvVar(name: string, required: true): string
-function getEnvVar(name: string, required: false): string|undefined
-function getEnvVar(name: string, required = true): string|undefined {
-  const value = process.env[name];
-  if (required && value === undefined) {
+function getEnvVar(name: string, fallback: string): string
+function getEnvVar(name: string, fallback: null): string|null
+function getEnvVar(name: string, fallback: string|null|undefined = undefined): string|null {
+  const value = process.env[name] || fallback;
+  if (value === undefined) {
     throw new Error(`Missing env variable ${name}`);
   }
   return value;
@@ -15,13 +15,28 @@ export enum NodeEnv {
   ci = 'CI'
 }
 
-export const config = {
+export interface Config {
+  ynabToken: string;
+  wfUrl: string;
+  wfUsername: string;
+  wfPassword: string;
+  captchaToken: string;
+  nodeEnv: NodeEnv;
+  useTracing: boolean;
+  debugBucketName: string|null;
+  browserTraceFilePath: string;
+  consoleTraceFilePath: string;
+}
+
+export const config: Config = {
   ynabToken: getEnvVar('YNAB_TOKEN'),
   wfUrl: getEnvVar('WF_URL'),
   wfUsername: getEnvVar('WF_USERNAME'),
   wfPassword: getEnvVar('WF_PASSWORD'),
   captchaToken: getEnvVar('CAPTCHA_TOKEN'),
-  nodeEnv: (getEnvVar('NODE_ENV', false) || NodeEnv.prod) as NodeEnv,
-  useTracing: getEnvVar('USE_TRACING', false) === 'true',
-  debugBucketName: getEnvVar('DEBUG_BUCKET_NAME', false)
+  nodeEnv: getEnvVar('NODE_ENV', NodeEnv.prod) as NodeEnv,
+  useTracing: getEnvVar('USE_TRACING', 'false') === 'true',
+  debugBucketName: getEnvVar('DEBUG_BUCKET_NAME', null),
+  browserTraceFilePath: getEnvVar('BROWSER_TRACE_FILE_PATH', 'trace.json'),
+  consoleTraceFilePath: getEnvVar('CONSOLE_TRACE_FILE_PATH', 'console.log'),
 };
